5f96d8dcbbc4d79f43c51bcc552dba925b7f0da8
ern19 <aarontrammell19@gmail.com>
2018-12-11 12:27:22 -0500

    set up router and skeleton components on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import ArtistList from './components/ArtistList'
+import Artist from './components/Artist'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div>
+            <h1>Tunr</h1>
+            <div>
+              <Link to="/">Artists</Link>
+              <Link to="/artist/1">Single Artist</Link>
+            </div>
+          </div>
+          <Route exact path="/" component={ArtistList}/>
+          <Route path="/artist/:id" component={Artist}/>
+        </div>  
+      </Router>
+
     );
   }
 }
---
+++ client/src/components/ArtistList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ArtistList extends Component {
+  render() {
+    return (
+      <div>
+        i am a list of artists
+      </div>
+    )
+  }
+}
